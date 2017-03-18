2a0748a3e38b8f3523d29405c4718a24d44b22d9
James Cockshull <james.cockshull@gmail.com>
2017-03-18 22:25:53 +0700

    basic hello world

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var del = require('del');
+var browserSync = require('browser-sync');
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+  browserSync.init({
+    server: "./src"
+  });
+  gulp.watch("src/*").on('change', browserSync.reload);
+});
+
+// Basic copy src -> dist
+gulp.task('copy', function() {
+  return gulp.src('src/*').pipe(gulp.dest('dist'))
+})
+
+// Clean the dist dir
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/*']);
+});
+
+// Build Sequences
+// ---------------
+gulp.task('default', ['serve']);
+gulp.task('build', ['clean:dist', 'copy'])
