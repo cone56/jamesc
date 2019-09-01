fb667ef70d5e00064c946de4bd01af453359eb1f
James Cockshull <james.cockshull@gmail.com>
2019-09-01 16:23:55 +0700

    Upgrade deps; use new gulp task API

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,21 @@
-var gulp = require('gulp');
-var del = require('del');
-var browserSync = require('browser-sync');
+const gulp = require('gulp');
+const del = require('del');
+const browserSync = require('browser-sync');
 
-// Static Server + watching scss/html files
-gulp.task('serve', function() {
-  browserSync.init({
-    server: "./src"
-  });
-  gulp.watch("src/*").on('change', browserSync.reload);
-});
+const {src, dest, watch, series} = gulp;
+
+// Static Server + watching src files
+const serve = () => {
+  browserSync.init({server: './src'});
+  watch('src/*').on('change', browserSync.reload);
+};
 
 // Basic copy src -> dist
-gulp.task('copy', function() {
-  return gulp.src('src/**/*', {dot: true}).pipe(gulp.dest('dist'))
-})
+const copy = () => src('src/**/*', {dot: true}).pipe(dest('dist'))
 
 // Clean the dist dir
-gulp.task('clean:dist', function() {
-  return del.sync(['dist/*']);
-});
+const clean = () => del(['dist/*']);
 
-// Build Sequences
-// ---------------
-gulp.task('default', ['serve']);
-gulp.task('build', ['clean:dist', 'copy'])
+exports.default = serve;
+exports.serve = serve;
+exports.build = series(clean, copy)
