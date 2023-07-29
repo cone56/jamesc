c239d480a1195d4c8267f698094e4943336858d0
James Cockshull <jc@bsd.education>
2023-07-29 23:47:00 +0100

    Add canonical link tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,8 @@ export const Layout = (props: { title?: string; children?: any }) => {
         <meta charset="utf-8" />
         <title>James Cockshull ${props.title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="/public/app.css" />
+        <link rel="canonical" href="https://jamesc.dev" />
+        <link rel="stylesheet" href="/app.css" />
       </head>
       <body
         class="font-mono leading-8 mx-[10vw] my-[10vh] dark:selection:bg-custom-yellow selection:bg-blue-700 selection:text-neutral-100 dark:selection:text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100 transition-all"
